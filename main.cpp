0828367db1b61d4f7e583cd902e88cb63f262dd8
Filip Žitný <filipzitny@gmail.com>
2023-09-27 09:38:37 +0200

    init

--- main.cpp
+++ main.cpp
@@ -4,17 +4,21 @@ const int N = 12;
 
 unsigned int money[N] = {5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 2, 1};
 
-void calculate(unsigned int val, int n, unsigned int res[]){
+void calculate(unsigned int val, int n, unsigned int currency[],unsigned int res[]){
     unsigned int p;
 
     for (int i = 0; i < n ; ++i) {
-        p = val / money[i];
+        p = val / currency[i];
         res[i] = p;
-        val = val - p * money[i];
+        val = val - p * currency[i];
     }
 }
 
-void print(unsigned int val, int n, unsigned int *res){
+void print(unsigned int val, int n, unsigned int *currency ,unsigned int *res){
+    std::cout << val << " = \n";
+    for (int i = 0; i < n; ++i) {
+        if(res[i] > 0)
+    }
 
 }
 
@@ -24,7 +28,9 @@ int main() {
 
 
     std::cout << "Enter value of your bill in CZK" << std::endl;
-    std::cin >> x;
+    std::cin >> amount;
+    calculate(amount, N, money, result);
+    print(amount, N, money, result);
 
     return 0;
 }
