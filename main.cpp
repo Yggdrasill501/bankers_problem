ab62a18fee391e96e19fd1b8a39a1e5297cd49ea
Filip Žitný <filipzitny@gmail.com>
2023-09-27 09:40:26 +0200

    done

--- main.cpp
+++ main.cpp
@@ -4,22 +4,23 @@ const int N = 12;
 
 unsigned int money[N] = {5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 2, 1};
 
-void calculate(unsigned int val, int n, unsigned int currency[],unsigned int res[]){
+void calculate(unsigned int val, int n, unsigned int currency_array[],unsigned int res[]){
     unsigned int p;
 
     for (int i = 0; i < n ; ++i) {
-        p = val / currency[i];
+        p = val / currency_array[i];
         res[i] = p;
-        val = val - p * currency[i];
+        val = val - p * currency_array[i];
     }
 }
 
 void print(unsigned int val, int n, unsigned int *currency ,unsigned int *res){
     std::cout << val << " = \n";
     for (int i = 0; i < n; ++i) {
-        if(res[i] > 0)
+        if(res[i] > 0){
+            std::cout << res[i] << " x " << currency[i] << std::endl;
+        }
     }
-
 }
 
 int main() {
