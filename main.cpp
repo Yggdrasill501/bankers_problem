71acf9a78b55760bfc69477515170b7908468e80
Filip Žitný <filipzitny@gmail.com>
2023-09-27 14:16:28 +0200

    failed

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ const int N = 12;
 
 unsigned int money[N] = {5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 2, 1};
 
-void calculate(unsigned int val, int n, unsigned int currency_array[],unsigned int res[]){
+void calculate(unsigned int val, int n,const unsigned int currency_array[],unsigned int res[]){
     unsigned int p;
 
     for (int i = 0; i < n ; ++i) {
@@ -14,11 +14,11 @@ void calculate(unsigned int val, int n, unsigned int currency_array[],unsigned i
     }
 }
 
-void print(unsigned int val, int n, unsigned int *currency ,unsigned int *res){
+void print(unsigned int val, int n, unsigned int *currency_array ,unsigned int *res){
     std::cout << val << " = \n";
     for (int i = 0; i < n; ++i) {
         if(res[i] > 0){
-            std::cout << res[i] << " x " << currency[i] << std::endl;
+            std::cout << res[i] << " x " << currency_array[i] << std::endl;
         }
     }
 }
